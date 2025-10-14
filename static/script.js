372269667bd43a740b1cdf6d5d26956cc28906d1
cjoaquin <joaquin.cozette@ynov.com>
2025-10-14 17:20:43 +0200

    ajout dark mode

---
+++ static/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  const toggle = document.querySelector(".switch input");
+
+  // Appliquer le thème sauvegardé
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "light") {
+    body.classList.add("light-mode");
+    body.classList.remove("dark-mode");
+    toggle.checked = false;
+  } else {
+    body.classList.add("dark-mode");
+    body.classList.remove("light-mode");
+    toggle.checked = true;
+  }
+
+  // Écoute du changement de thème
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      body.classList.add("dark-mode");
+      body.classList.remove("light-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      body.classList.add("light-mode");
+      body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
